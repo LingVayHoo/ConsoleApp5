d7695a3949c0dc5eb903ab7ed929fd9d524d9fad
Andrey Shevchenko <vakarianx@yandex.ru>
2023-12-16 22:14:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string text = Console.ReadLine();
+            string[] textArray = SplitText(text);
+            PrintTextArray(textArray);
+            Console.ReadKey();
+            //PrintTextArray(SplitText(text)); я бы сделал так
+        }
+
+        static string[] SplitText(string text)
+        {
+            string[] textArray = text.Split(' ');
+            return textArray;
+        }
+
+        static void PrintTextArray(string[] textArray)
+        {
+            for (int i = 0; i < textArray.Length; i++)
+            {
+                Console.WriteLine(textArray[i]);
+            }
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string text = Console.ReadLine();
+            Console.WriteLine(Reverse(text));
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Метод разделения предложения на массив слов
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static string[] SplitText(string text)
+        {
+            string[] textArray = text.Split(' ');
+            return textArray;
+        }
+
+        /// <summary>
+        /// Метод изменения порядка слов на обратный
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static string Reverse(string text)
+        {
+            string[] textArray = SplitText(text);
+            Array.Reverse(textArray);
+
+            string reversedText = null;
+            for (int i = 0; i < textArray.Length; i++)
+            {
+                reversedText += textArray[i] + ' ';
+            }
+            return reversedText;
+        }
+    }
+}
